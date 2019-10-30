225a7cf7672b6a4e890044dd674937e9f3a7328e
MountainRains <lushanyu1996@gmail.com>
2019-10-30 10:33:49 +0800

    8.7

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,65 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+string version1(const string& s1, const string& s2);
+const string& version2( string& s1, const string& s2);
+const string& version3( string& s1, const string& s2);
+
+
+int main()
+{
+    string input;
+    string copy;
+    string result;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+    copy = input;
+    cout << "Your string as entered: " << input << endl;
+    result = version1(input, "***");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    result = version2(input, "###");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+    result = version3(input, "@@@");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    return 0;
+}
+
+
+string version1(const string& s1, const string& s2)
+{
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+}
+const string& version2( string& s1, const string& s2)
+{
+    s1 = s2 + s1 + s2;
+    return s1;
+}
+const string& version3( string& s1, const string& s2)
+{
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+
+}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
